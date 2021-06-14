d54dcb00bec3865db26043ff55e2ac637b32c282
Rinafhnhri <bailuanzhang8954ox@126.com>
2021-06-14 11:55:18 +0800

    添加所有的子视图

--- Weibo/Weibo/Classes/Module/Main/MainViewController.swift
+++ Weibo/Weibo/Classes/Module/Main/MainViewController.swift
@@ -6,10 +6,15 @@
 //  Copyright © 2015年 itcast. All rights reserved.
 //
 
+//格式化代码
+//control + i
+// cmd + a -> cmd + x -> cmd + v
+
+//cmd + shift + j 快速聚焦
 import UIKit
 
 class MainViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,13 +25,27 @@ class MainViewController: UITabBarController {
     
     //private表示只能在本类中进行访问
     private func addChildViewControllers() {
-        let homeVC = HomeTableViewController()
-        let nav = UINavigationController(rootViewController: homeVC)
-//        title = "首页"
-//        nav.title = "首页"
-        homeVC.title = "首页"
+        //HomeTableViewController() 需要修改
+        addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
+        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
+        addChildViewController(DiscoverTableViewController(), title: "发现", imageName: "tabbar_discover")
+        addChildViewController(ProfileTableViewController(), title: "我", imageName: "tabbar_profile")
+        
+    }
+    
+    private func addChildViewController(vc: UIViewController,title: String,imageName: String) {
+        self.tabBar.tintColor = UIColor.orangeColor()
+        
+        let nav = UINavigationController(rootViewController: vc)
+        //        title = "首页"
+        //        nav.title = "首页"
+        vc.title = title
+        //设置图片
+        vc.tabBarItem.image = UIImage(named: imageName)
+//        vc.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+//        vc.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.orangeColor()], forState: .Selected)
         //在swift中 self 可以省略
         self.addChildViewController(nav)
     }
-
+    
 }
